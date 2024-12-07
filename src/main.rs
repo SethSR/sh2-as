061bcbbe51c0284ebdf8f233f50d7c54f7aadca9
srenshaw <srenshaw1@gmail.com>
2024-12-07 10:52:15 -0600

    Added  and  functions to simplify parser.

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,29 @@ fn main() -> miette::Result<()> {
 	let file = std::fs::read_to_string(source)
 		.into_diagnostic()?;
 
-	fn next_line(
-		chars: &mut impl Iterator<Item = (usize,char)>,
+	fn tokenize<I: Iterator<Item=(usize,char)>>(
 		cur_idx: usize,
-		line_idx: &mut usize,
+		chars: &mut std::iter::Peekable<I>,
 		char_idx: &mut usize,
-	) -> (usize,usize) {
+		pred: fn(char) -> bool,
+	) -> usize {
+		let mut index = cur_idx + 1;
+		while let Some((idx,ch)) = chars.peek() {
+			index = *idx;
+			if !pred(*ch) {
+				break;
+			}
+			next(char_idx, chars);
+		}
+		index - cur_idx
+	}
+
+	fn next_line(
+		cur_idx: usize,
+		chars: &mut impl Iterator<Item = (usize,char)>,
+		char_idx: &mut usize,
+		line_idx: &mut usize,
+	) -> usize {
 		let mut index = cur_idx + 1;
 		while let Some((cmt_idx,cmt_char)) = chars.next() {
 			*char_idx += 1;
@@ -29,7 +46,7 @@ fn main() -> miette::Result<()> {
 				break;
 			}
 		}
-		(cur_idx, (index - cur_idx))
+		index - cur_idx
 	}
 
 	let mut results = Vec::new();
@@ -37,201 +54,194 @@ fn main() -> miette::Result<()> {
 	let mut char_idx = 0;
 	let mut chars = file.char_indices().peekable();
 
+	fn next(
+		idx: &mut usize,
+		chars: &mut impl Iterator<Item=(usize,char)>,
+	) {
+		*idx += 1;
+		chars.next();
+	}
+
 	use TokenType::*;
 	while let Some(&(cur_idx, cur_char)) = chars.peek() {
 		match cur_char {
-			' ' | '\t' => {
-				char_idx += 1;
-				chars.next();
-			}
+			' ' | '\t' => next(&mut char_idx, &mut chars),
 			'\n' => {
 				line_idx += 1;
 				char_idx = 0;
 				chars.next();
 			}
 			',' => {
-				char_idx += 1;
-				chars.next();
+				next(&mut char_idx, &mut chars);
 				results.push(Token::new(Comma, cur_idx, 1));
 			}
 			'+' => {
-				char_idx += 1;
-				chars.next();
+				next(&mut char_idx, &mut chars);
 				results.push(Token::new(Plus, cur_idx, 1));
 			}
 			'-' => {
-				char_idx += 1;
-				chars.next();
+				next(&mut char_idx, &mut chars);
 				results.push(Token::new(Dash, cur_idx, 1));
 			}
 			'@' => {
-				char_idx += 1;
-				chars.next();
+				next(&mut char_idx, &mut chars);
 				results.push(Token::new(Address, cur_idx, 1));
 			}
 			'#' => {
-				char_idx += 1;
-				chars.next();
+				next(&mut char_idx, &mut chars);
 				results.push(Token::new(Immediate, cur_idx, 1));
 			}
 			'0'..='9' => {
-				char_idx += 1;
-				chars.next();
-				let mut index = cur_idx + 1;
-				while let Some((idx,ch)) = chars.peek() {
-					index = *idx;
-					if !('0'..='9').contains(ch) && '_' != *ch {
-						break;
-					}
-					char_idx += 1;
-					chars.next();
-				}
-				results.push(Token::new(Number, cur_idx, index - cur_idx));
+				next(&mut char_idx, &mut chars);
+				let size = tokenize(
+					cur_idx, &mut chars, &mut char_idx,
+					|ch| ('0'..='9').contains(&ch) || '_' == ch);
+				results.push(Token::new(Number, cur_idx, size));
 			}
 			'$' => {
-				char_idx += 1;
-				chars.next();
-				let mut index = cur_idx + 1;
-				while let Some((idx,ch)) = chars.peek() {
-					index = *idx;
-					if !('0'..='9').contains(ch)
-					&& !('a'..='f').contains(ch)
-					&& !('A'..='F').contains(ch)
-					&& '_' != *ch
-					{
-						break;
-					}
-					char_idx += 1;
-					chars.next();
-				}
-				results.push(Token::new(Number, cur_idx, index - cur_idx));
+				next(&mut char_idx, &mut chars);
+				let size = tokenize(
+					cur_idx, &mut chars, &mut char_idx,
+					|ch| ('0'..='9').contains(&ch)
+						|| ('a'..='f').contains(&ch)
+						|| ('A'..='F').contains(&ch)
+						|| '_' == ch
+				);
+				results.push(Token::new(Number, cur_idx, size));
 			}
 			'%' => {
-				char_idx += 1;
-				chars.next();
-				let mut index = cur_idx + 1;
-				while let Some((idx,ch)) = chars.peek() {
-					index = *idx;
-					if !['0', '1'].contains(ch) && '_' != *ch {
-						break;
-					}
-					char_idx += 1;
-					chars.next();
-				}
-				results.push(Token::new(Number, cur_idx, index - cur_idx));
+				next(&mut char_idx, &mut chars);
+				let size = tokenize(
+					cur_idx, &mut chars, &mut char_idx,
+					|ch| ['0','1'].contains(&ch) || '_' == ch);
+				results.push(Token::new(Number, cur_idx, size));
 			}
 			'r' => {
-				char_idx += 1;
-				chars.next();
-				let mut index = cur_idx + 1;
-				while let Some((idx,ch)) = chars.peek() {
-					index = *idx;
-					if !('0'..='9').contains(ch) {
-						break;
-					}
-					char_idx += 1;
-					chars.next();
-				}
-				results.push(Token::new(Register, cur_idx, index - cur_idx));
+				next(&mut char_idx, &mut chars);
+				let size = tokenize(
+					cur_idx, &mut chars, &mut char_idx,
+					|ch| ('0'..='9').contains(&ch));
+				results.push(Token::new(Register, cur_idx, size));
 			}
 			'a' => {
-				char_idx += 1;
-				chars.next();
+				next(&mut char_idx, &mut chars);
 				let Some((_,'d')) = chars.peek() else {
-					let (idx,size) = next_line(&mut chars, cur_idx, &mut line_idx, &mut char_idx);
+					let size = next_line(
+						cur_idx, &mut chars,
+						&mut char_idx, &mut line_idx);
 					char_idx += size;
-					results.push(Token::new(Unknown(line_idx,char_idx), idx, size));
+					results.push(Token::new(
+						Unknown(line_idx,char_idx), cur_idx, size));
 					break;
 				};
-				char_idx += 1;
-				chars.next();
+				next(&mut char_idx, &mut chars);
 				let Some(&(idx,'d')) = chars.peek() else {
-					let (idx,size) = next_line(&mut chars, cur_idx, &mut line_idx, &mut char_idx);
+					let size = next_line(
+						cur_idx, &mut chars,
+						&mut char_idx, &mut line_idx);
 					char_idx += size;
-					results.push(Token::new(Unknown(line_idx,char_idx), idx, size));
+					results.push(Token::new(
+						Unknown(line_idx,char_idx), cur_idx, size));
 					break;
 				};
-				char_idx += 1;
-				chars.next();
-				results.push(Token::new(Add, cur_idx, idx - cur_idx + 1));
+				next(&mut char_idx, &mut chars);
+				results.push(Token::new(
+					Add, cur_idx, idx - cur_idx + 1));
 			}
 			'm' => {
-				char_idx += 1;
-				chars.next();
+				next(&mut char_idx, &mut chars);
 				let Some((_,'o')) = chars.peek() else {
-					let (idx,size) = next_line(&mut chars, cur_idx, &mut line_idx, &mut char_idx);
-					results.push(Token::new(Unknown(line_idx,char_idx), idx, size));
+					let size = next_line(
+						cur_idx, &mut chars,
+						&mut char_idx, &mut line_idx);
+					results.push(Token::new(
+						Unknown(line_idx,char_idx), cur_idx, size));
 					break;
 				};
-				char_idx += 1;
-				chars.next();
+				next(&mut char_idx, &mut chars);
 				let Some((_,'v')) = chars.peek() else {
-					let (idx,size) = next_line(&mut chars, cur_idx, &mut line_idx, &mut char_idx);
-					results.push(Token::new(Unknown(line_idx,char_idx), idx, size));
+					let size = next_line(
+						cur_idx, &mut chars,
+						&mut char_idx, &mut line_idx);
+					results.push(Token::new(
+						Unknown(line_idx,char_idx), cur_idx, size));
 					break;
 				};
-				char_idx += 1;
-				chars.next();
+				next(&mut char_idx, &mut chars);
 				let Some((_,'.')) = chars.peek() else {
-					let (idx,size) = next_line(&mut chars, cur_idx, &mut line_idx, &mut char_idx);
-					results.push(Token::new(Unknown(line_idx,char_idx), idx, size));
+					let size = next_line(
+						cur_idx, &mut chars,
+						&mut char_idx, &mut line_idx);
+					results.push(Token::new(
+						Unknown(line_idx,char_idx), cur_idx, size));
 					break;
 				};
-				char_idx += 1;
-				chars.next();
+				next(&mut char_idx, &mut chars);
 				match chars.peek() {
 					Some(&(idx,'b')) => {
-						char_idx += 1;
-						chars.next();
-						results.push(Token::new(MovB, cur_idx, idx - cur_idx + 1));
+						next(&mut char_idx, &mut chars);
+						results.push(Token::new(
+							MovB, cur_idx, idx - cur_idx + 1));
 					}
 					Some(&(idx,'w')) => {
-						char_idx += 1;
-						chars.next();
-						results.push(Token::new(MovW, cur_idx, idx - cur_idx + 1));
+						next(&mut char_idx, &mut chars);
+						results.push(Token::new(
+							MovW, cur_idx, idx - cur_idx + 1));
 					}
 					Some(&(idx,'l')) => {
-						char_idx += 1;
-						chars.next();
-						results.push(Token::new(MovL, cur_idx, idx - cur_idx + 1));
+						next(&mut char_idx, &mut chars);
+						results.push(Token::new(
+							MovL, cur_idx, idx - cur_idx + 1));
 					}
 					_ => {
-						let (idx,size) = next_line(&mut chars, cur_idx, &mut line_idx, &mut char_idx);
+						let size = next_line(
+							cur_idx, &mut chars,
+							&mut char_idx, &mut line_idx);
 						char_idx += size;
-						results.push(Token::new(Unknown(line_idx,char_idx), idx, size));
+						results.push(Token::new(
+							Unknown(line_idx,char_idx), cur_idx, size));
 					}
 				}
 			}
 			'o' => {
-				char_idx += 1;
-				chars.next();
+				next(&mut char_idx, &mut chars);
 				let Some((_,'r')) = chars.peek() else {
-					let (idx,size) = next_line(&mut chars, cur_idx, &mut line_idx, &mut char_idx);
+					let size = next_line(
+						cur_idx, &mut chars,
+						&mut char_idx, &mut line_idx);
 					char_idx += size;
-					results.push(Token::new(Unknown(line_idx,char_idx), idx, size));
+					results.push(Token::new(
+						Unknown(line_idx,char_idx), cur_idx, size));
 					break;
 				};
-				char_idx += 1;
-				chars.next();
+				next(&mut char_idx, &mut chars);
 				let Some(&(idx,'g')) = chars.peek() else {
-					let (idx,size) = next_line(&mut chars, cur_idx, &mut line_idx, &mut char_idx);
+					let size = next_line(
+						cur_idx, &mut chars,
+						&mut char_idx, &mut line_idx);
 					char_idx += size;
-					results.push(Token::new(Unknown(line_idx,char_idx), idx, size));
+					results.push(Token::new(
+						Unknown(line_idx,char_idx), cur_idx, size));
 					break;
 				};
-				char_idx += 1;
-				chars.next();
-				results.push(Token::new(Org, cur_idx, idx - cur_idx + 1));
+				next(&mut char_idx, &mut chars);
+				results.push(Token::new(
+					Org, cur_idx, idx - cur_idx + 1));
 			}
 			';' => {
-				let (idx,size) = next_line(&mut chars, cur_idx, &mut line_idx, &mut char_idx);
+				let size = next_line(
+					cur_idx, &mut chars,
+					&mut char_idx, &mut line_idx);
 				char_idx += size;
-				results.push(Token::new(Comment, idx, size));
+				results.push(Token::new(Comment, cur_idx, size));
 			}
 			_ => {
-				let (idx,size) = next_line(&mut chars, cur_idx, &mut line_idx, &mut char_idx);
+				let size = next_line(
+					cur_idx, &mut chars,
+					&mut char_idx, &mut line_idx);
 				char_idx += size;
-				results.push(Token::new(Unknown(line_idx,char_idx), idx, size));
+				results.push(Token::new(
+					Unknown(line_idx,char_idx), cur_idx, size));
 			}
 		}
 	}
