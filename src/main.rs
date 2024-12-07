c82a103f14f7b072e31c9ac6eac1c3b7a1dc436c
srenshaw <srenshaw1@gmail.com>
2024-12-07 13:21:11 -0600

    Added several new token-types, added an  function for identifier tokens, and moved the lexing code into a separate function.

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,36 @@ fn main() -> miette::Result<()> {
 	let target = args.next()
 		.unwrap_or("asm.out".to_string());
 
+	// TODO - srenshaw - Change this to a CLI option.
+	let is_silent = true;
+
 	let file = std::fs::read_to_string(source)
 		.into_diagnostic()?;
+
+	let results = lexer(&file);
+
+	for result in results {
+		let (idx,sz) = (result.index, result.size);
+		let out = format!("{:?}\t'{}'", result.tt,
+			&file[idx as usize..][..sz as usize]);
+		if let TokenType::Unknown(ln,ch) = result.tt {
+			println!("{out} ({ln},{ch})");
+		} else if !is_silent {
+			println!("{out}");
+		}
+	}
+
+	todo!("output to '{target}'");
+}
+
+fn lexer(input: &str) -> Vec<Token> {
+	fn next(
+		idx: &mut usize,
+		chars: &mut impl Iterator<Item=(usize,char)>,
+	) {
+		*idx += 1;
+		chars.next();
+	}
 
 	fn tokenize<I: Iterator<Item=(usize,char)>>(
 		cur_idx: usize,
@@ -30,9 +58,9 @@ fn main() -> miette::Result<()> {
 		index - cur_idx
 	}
 
-	fn next_line(
+	fn next_line<I: Iterator<Item=(usize,char)>>(
 		cur_idx: usize,
-		chars: &mut impl Iterator<Item = (usize,char)>,
+		chars: &mut I,
 		char_idx: &mut usize,
 		line_idx: &mut usize,
 	) -> usize {
@@ -49,18 +77,35 @@ fn main() -> miette::Result<()> {
 		index - cur_idx
 	}
 
+	fn ident<I: Iterator<Item=(usize,char)>>(
+		id: &str,
+		cur_idx: usize,
+		chars: &mut std::iter::Peekable<I>,
+		char_idx: &mut usize,
+		line_idx: &mut usize,
+	) -> Result<usize,usize> {
+		let mut idx = cur_idx;
+		for ch in id.chars() {
+			match chars.peek() {
+				Some(&(i,c)) if c == ch => {
+					idx = i;
+					next(char_idx, chars);
+				}
+				_ => {
+					let size = next_line(
+						cur_idx, chars, char_idx, line_idx);
+					*char_idx += size;
+					return Err(size);
+				}
+			}
+		}
+		Ok(idx - cur_idx + 1)
+	}
+
 	let mut results = Vec::new();
 	let mut line_idx = 0;
 	let mut char_idx = 0;
-	let mut chars = file.char_indices().peekable();
-
-	fn next(
-		idx: &mut usize,
-		chars: &mut impl Iterator<Item=(usize,char)>,
-	) {
-		*idx += 1;
-		chars.next();
-	}
+	let mut chars = input.char_indices().peekable();
 
 	use TokenType::*;
 	while let Some(&(cur_idx, cur_char)) = chars.peek() {
@@ -87,9 +132,17 @@ fn main() -> miette::Result<()> {
 				next(&mut char_idx, &mut chars);
 				results.push(Token::new(Address, cur_idx, 1));
 			}
+			':' => {
+				next(&mut char_idx, &mut chars);
+				results.push(Token::new(Colon, cur_idx, 1));
+			}
 			'#' => {
 				next(&mut char_idx, &mut chars);
 				results.push(Token::new(Immediate, cur_idx, 1));
+			}
+			'.' => {
+				next(&mut char_idx, &mut chars);
+				results.push(Token::new(Dot, cur_idx, 1));
 			}
 			'0'..='9' => {
 				next(&mut char_idx, &mut chars);
@@ -123,110 +176,34 @@ fn main() -> miette::Result<()> {
 					|ch| ('0'..='9').contains(&ch));
 				results.push(Token::new(Register, cur_idx, size));
 			}
-			'a' => {
-				next(&mut char_idx, &mut chars);
-				let Some((_,'d')) = chars.peek() else {
-					let size = next_line(
-						cur_idx, &mut chars,
-						&mut char_idx, &mut line_idx);
-					char_idx += size;
-					results.push(Token::new(
-						Unknown(line_idx,char_idx), cur_idx, size));
-					break;
-				};
-				next(&mut char_idx, &mut chars);
-				let Some(&(idx,'d')) = chars.peek() else {
-					let size = next_line(
-						cur_idx, &mut chars,
-						&mut char_idx, &mut line_idx);
-					char_idx += size;
-					results.push(Token::new(
-						Unknown(line_idx,char_idx), cur_idx, size));
-					break;
-				};
-				next(&mut char_idx, &mut chars);
-				results.push(Token::new(
-					Add, cur_idx, idx - cur_idx + 1));
+			'a' => match ident("add",
+				cur_idx, &mut chars,
+				&mut char_idx, &mut line_idx,
+			) {
+				Ok(sz) => results.push(Token::new(
+					Add, cur_idx, sz)),
+				Err(sz) => results.push(Token::new(
+					Unknown(line_idx,char_idx), cur_idx, sz)),
 			}
 			'm' => {
-				next(&mut char_idx, &mut chars);
-				let Some((_,'o')) = chars.peek() else {
-					let size = next_line(
-						cur_idx, &mut chars,
-						&mut char_idx, &mut line_idx);
-					results.push(Token::new(
-						Unknown(line_idx,char_idx), cur_idx, size));
-					break;
-				};
-				next(&mut char_idx, &mut chars);
-				let Some((_,'v')) = chars.peek() else {
-					let size = next_line(
-						cur_idx, &mut chars,
-						&mut char_idx, &mut line_idx);
-					results.push(Token::new(
-						Unknown(line_idx,char_idx), cur_idx, size));
-					break;
-				};
-				next(&mut char_idx, &mut chars);
-				let Some((_,'.')) = chars.peek() else {
-					let size = next_line(
-						cur_idx, &mut chars,
-						&mut char_idx, &mut line_idx);
-					results.push(Token::new(
-						Unknown(line_idx,char_idx), cur_idx, size));
-					break;
-				};
-				next(&mut char_idx, &mut chars);
-				match chars.peek() {
-					Some(&(idx,'b')) => {
-						next(&mut char_idx, &mut chars);
-						results.push(Token::new(
-							MovB, cur_idx, idx - cur_idx + 1));
-					}
-					Some(&(idx,'w')) => {
-						next(&mut char_idx, &mut chars);
-						results.push(Token::new(
-							MovW, cur_idx, idx - cur_idx + 1));
-					}
-					Some(&(idx,'l')) => {
-						next(&mut char_idx, &mut chars);
-						results.push(Token::new(
-							MovL, cur_idx, idx - cur_idx + 1));
-					}
-					_ => {
-						let size = next_line(
-							cur_idx, &mut chars,
-							&mut char_idx, &mut line_idx);
-						char_idx += size;
-						results.push(Token::new(
-							Unknown(line_idx,char_idx), cur_idx, size));
-					}
+				match ident("mov",
+					cur_idx, &mut chars,
+					&mut char_idx, &mut line_idx,
+				) {
+					Ok(sz) => results.push(Token::new(
+						Mov, cur_idx, sz)),
+					Err(sz) => results.push(Token::new(
+						Unknown(line_idx,char_idx), cur_idx, sz)),
 				}
 			}
-			'o' => {
-				next(&mut char_idx, &mut chars);
-				let Some((_,'r')) = chars.peek() else {
-					let size = next_line(
-						cur_idx, &mut chars,
-						&mut char_idx, &mut line_idx);
-					char_idx += size;
-					results.push(Token::new(
-						Unknown(line_idx,char_idx), cur_idx, size));
-					break;
-				};
-				next(&mut char_idx, &mut chars);
-				let Some(&(idx,'g')) = chars.peek() else {
-					let size = next_line(
-						cur_idx, &mut chars,
-						&mut char_idx, &mut line_idx);
-					char_idx += size;
-					results.push(Token::new(
-						Unknown(line_idx,char_idx), cur_idx, size));
-					break;
-				};
-				next(&mut char_idx, &mut chars);
-				results.push(Token::new(
-					Org, cur_idx, idx - cur_idx + 1));
+			'o' => match ident("org",
+				cur_idx, &mut chars,
+				&mut char_idx, &mut line_idx)
+			{
+				Ok(idx) => results.push(Token::new(
+					Org, cur_idx, idx)),
+				Err(sz) => results.push(Token::new(
+					Unknown(line_idx,char_idx), cur_idx, sz)),
 			}
 			';' => {
 				let size = next_line(
@@ -234,6 +211,14 @@ fn main() -> miette::Result<()> {
 					&mut char_idx, &mut line_idx);
 				char_idx += size;
 				results.push(Token::new(Comment, cur_idx, size));
+			}
+			c if c.is_alphabetic() => {
+				let size = tokenize(
+					cur_idx, &mut chars, &mut char_idx,
+					|ch| ('a'..='z').contains(&ch)
+						|| ('A'..='Z').contains(&ch)
+						|| '_' == ch);
+				results.push(Token::new(Identifier,cur_idx,size));
 			}
 			_ => {
 				let size = next_line(
@@ -246,13 +231,7 @@ fn main() -> miette::Result<()> {
 		}
 	}
 
-	for result in results {
-		print!("{:?}", result.tt);
-		let (idx,sz) = (result.index, result.size);
-		println!("\t'{}'", &file[idx as usize..][..sz as usize]);
-	}
-
-	Ok(())
+	results
 }
 
 #[derive(Debug)]
@@ -272,16 +251,17 @@ impl Token {
 enum TokenType {
 	Comment,
 	Org,
-	MovB,
-	MovW,
-	MovL,
+	Mov,
 	Add,
 	Plus,
 	Dash,
 	Register,
 	Comma,
+	Dot,
 	Immediate,
 	Number,
+	Identifier,
+	Colon,
 	Address,
 	Unknown(usize,usize),
 }
