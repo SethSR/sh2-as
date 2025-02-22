c6dabd42c2ca533938d992b70dd0d9fbb4bac66e
srenshaw <srenshaw1@gmail.com>
2025-02-22 03:04:33 -0600

    Updated grammar for easier error parsing. Added new tests to check for simple error conditions.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
 
-use std::fs::read_to_string;
-
 use pest::Parser;
 use pest::iterators::Pair;
 use pest_derive::Parser;
@@ -30,20 +28,66 @@ impl Output {
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Asm {
 	ClrMac,
+	ClrT,
+}
+
+fn extra_rules(src: Pair<Rule>) {
+	#[cfg(not(test))]
+	error!("unexpected {src} - '{}'", src.as_str());
+
+	#[cfg(test)]
+	panic!("unexpected {src} - '{}'", src.as_str());
+}
+
+fn rename_rules(rule: &Rule) -> String {
+	match rule {
+		Rule::EOI => "EOF",
+		Rule::ins_line | Rule::ins => "instruction",
+		Rule::dir_line | Rule::dir => "assembler directive",
+		Rule::lbl_line => "Label",
+		Rule::val_line => "Value",
+		_ => unreachable!("{rule:?}"),
+	}.to_owned()
 }
 
 #[instrument]
 pub fn parser(input: &str) -> ParseResult<Output> {
 	let mut output = Output::default();
-	let source = Sh2Parser::parse(Rule::program, input)?;
+	let source = Sh2Parser::parse(Rule::program, input)
+		.map_err(|e| e.renamed_rules(rename_rules))?;
 	for src in source {
 		trace!("Parsing: {src} - '{}'", src.as_str());
+		output = match src.as_rule() {
+			Rule::ins_line => parse_ins_line(src, output)?,
+			Rule::dir_line => todo!("{src} - '{}'", src.as_str()),
+			Rule::lbl_line => todo!("{src} - '{}'", src.as_str()),
+			Rule::val_line => todo!("{src} - '{}'", src.as_str()),
+			Rule::EOI => continue,
+			_ => {
+				extra_rules(src);
+				continue;
+			}
+		};
+	}
+	Ok(output)
+}
+
+#[instrument]
+fn parse_ins_line(source: Pair<Rule>, mut output: Output) -> ParseResult<Output> {
+	let source = source.into_inner().next().unwrap();
+	for src in source.into_inner() {
+		trace!("{src} - '{}'", src.as_str());
+
 		match src.as_rule() {
 			Rule::ins_clrmac => {
 				output.push(Asm::ClrMac);
 			}
+			Rule::ins_clrt => {
+				output.push(Asm::ClrT);
+			}
 			_ => {
-				error!("unexpected {src} - '{}'", src.as_str());
+				extra_rules(src);
+				continue;
 			}
 		}
 	}
@@ -54,15 +98,45 @@ pub fn parser(input: &str) -> ParseResult<Output> {
 mod parser {
 	use super::*;
 
+	macro_rules! test_single {
+		($input:expr, $asm:expr) => {
+			let out = super::parser($input)
+				.map_err(|e| panic!("{e}"))
+				.unwrap();
+			assert_eq!(out.0, vec![$asm]);
+		}
+	}
+
 	#[test]
 	fn clrmac() {
-		let input = "\tclrmac";
-		let out = parser(input)
-			.map_err(|e| panic!("{e}"))
-			.unwrap();
-		assert_eq!(out.0, vec![
-			Asm::ClrMac,
-		]);
+		test_single!("\tclrmac", Asm::ClrMac);
+	}
+
+	#[test]
+	fn clrt() {
+		test_single!("\tclrt", Asm::ClrT);
+	}
+
+	#[test]
+	#[should_panic = " --> 1:1
+  |
+1 | stuff
+  | ^---
+  |
+  = expected EOF, instruction, assembler directive, Label, or Value"]
+	fn failure_no_starting_tab() {
+		test_single!("stuff", Asm::ClrT);
+	}
+
+	#[test]
+	#[should_panic = " --> 1:2
+  |
+1 | 	stuff
+  | 	^---
+  |
+  = expected instruction or assembler directive"]
+	fn failure_non_command() {
+		test_single!("\tstuff", Asm::ClrT);
 	}
 }
 
@@ -71,26 +145,35 @@ fn output(asm: &[Asm]) -> Vec<u8> {
 	let mut out = Vec::with_capacity(asm.len());
 	for cmd in asm {
 		match cmd {
-			Asm::ClrMac => out.extend(&[0x00, 0x00, 0x02, 0x80]),
+			Asm::ClrMac => out.push(0x0028),
+			Asm::ClrT => out.push(0x0008),
 		}
 	}
-	out
+	out.into_iter()
+		.flat_map(|word: u16| word.to_be_bytes())
+		.collect()
 }
 
 #[cfg(test)]
 mod output {
 	use super::*;
 
-	#[test]
-	fn clrmac() {
-		let input = "\tclrmac";
+	fn test_output(input: &str, bytes: &[u8]) {
 		let asm = parser(input)
 			.map_err(|e| panic!("{e}"))
 			.unwrap();
 		let out = output(&asm.0);
-		assert_eq!(out, vec![
-			0x00, 0x00, 0x02, 0x80,
-		]);
+		assert_eq!(out, bytes);
+	}
+
+	#[test]
+	fn clrmac() {
+		test_output("\tclrmac", &[0x00, 0x28]);
+	}
+
+	#[test]
+	fn clrt() {
+		test_output("\tclrt", &[0x00, 0x08]);
 	}
 }
 
