b6acee438d7127fbbecc8291f46f9d8ace17e2d3
srenshaw <srenshaw1@gmail.com>
2025-06-23 02:36:00 -0500

    Fixed more clippy issues

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,7 @@ fn main() {
 		.expect("no source file name");
 	let file_path = PathBuf::from(&file_name);
 	let file_path = file_path.parent()
-		.expect(&format!("unable to parse parent file-path for '{file_name}'"));
+		.unwrap_or_else(|| panic!("unable to parse parent file-path for '{file_name}'"));
 
 	let use_parser2 = args.next()
 		.map(|arg| arg == "p2")
@@ -36,13 +36,12 @@ fn main() {
 
 	let source = fs::read_to_string(file_name)
 		.expect("unable to read source file");
+
 	if use_parser2 {
 		let tokens = lexer::eval(&source)
 			.expect("unable to lex source (v2)");
-		let out = p3::eval(&tokens, file_path.to_path_buf())
-			//.expect("unable to parse source (v2)")
-			;
-		todo!("{out:?}");
+		let out = p3::eval(&tokens, file_path.to_path_buf());
+		println!("{out:02X?}");
 	} else {
 		let ins = parser(&source)
 			.expect("unable to parse source");
